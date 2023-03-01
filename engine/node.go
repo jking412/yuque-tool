bba9176d0e18b40c84122fb06e1efa68937832ce
jking412 <3250532948@qq.com>
2023-03-02 02:33:22 +0800

    refactor: Modify the Node interface to add more functional functions

--- engine/node.go
+++ engine/node.go
@@ -1,6 +1,12 @@
 package engine
 
+import "fmt"
+
 type Node interface {
+	Type() NodeType
+	String() string
+	Position() Pos
+	tree() *Tree
 }
 
 type NodeType int
@@ -10,16 +16,41 @@ const (
 	NodeTable
 	NodeTableField
 	NodeList
-	NodeRepeat
-	NodeLeftDelim
-	NodeRightDelim
 )
+
+// 验证TextNode是否实现Node接口
+var _ Node = (*TextNode)(nil)
 
 type TextNode struct {
 	NodeType
 	Pos
 	tr   *Tree
 	Text []byte
+}
+
+func (t *Tree) newText(pos Pos, text []byte) *TextNode {
+	return &TextNode{
+		NodeType: NodeText,
+		Pos:      pos,
+		Text:     text,
+		tr:       t,
+	}
+}
+
+func (t *TextNode) Type() NodeType {
+	return t.NodeType
+}
+
+func (t *TextNode) Position() Pos {
+	return t.Pos
+}
+
+func (t *TextNode) String() string {
+	return fmt.Sprintf("%s", t.Text)
+}
+
+func (t *TextNode) tree() *Tree {
+	return t.tr
 }
 
 type TableNode struct {
@@ -31,6 +62,33 @@ type TableNode struct {
 	Repeated bool
 }
 
+func (t *Tree) newTable(pos Pos, name string) *TableNode {
+	return &TableNode{
+		NodeType: NodeTable,
+		Pos:      pos,
+		tr:       t,
+		Name:     name,
+		Repeated: false,
+		Fields:   make([]*TableFieldNode, 0),
+	}
+}
+
+func (t *TableNode) Type() NodeType {
+	return t.NodeType
+}
+
+func (t *TableNode) Position() Pos {
+	return t.Pos
+}
+
+func (t *TableNode) String() string {
+	return fmt.Sprintf("%s", t.Name)
+}
+
+func (t *TableNode) tree() *Tree {
+	return t.tr
+}
+
 type TableFieldNode struct {
 	NodeType
 	Pos
@@ -38,9 +96,59 @@ type TableFieldNode struct {
 	Val string
 }
 
+func (t *Tree) newTableField(pos Pos, val string) *TableFieldNode {
+	return &TableFieldNode{
+		NodeType: NodeTableField,
+		Pos:      pos,
+		tr:       t,
+		Val:      val,
+	}
+}
+
+func (t *TableFieldNode) Type() NodeType {
+	return t.NodeType
+}
+
+func (t *TableFieldNode) Position() Pos {
+	return t.Pos
+}
+
+func (t *TableFieldNode) String() string {
+	return fmt.Sprintf("%s", t.Val)
+}
+
+func (t *TableFieldNode) tree() *Tree {
+	return t.tr
+}
+
 type ListNode struct {
 	NodeType
 	Pos
 	tr    *Tree
 	Nodes []Node
+}
+
+func (t *Tree) newList(pos Pos) *ListNode {
+	return &ListNode{
+		NodeType: NodeList,
+		Pos:      pos,
+		tr:       t,
+		Nodes:    make([]Node, 0),
+	}
+}
+
+func (t *ListNode) Type() NodeType {
+	return t.NodeType
+}
+
+func (t *ListNode) Position() Pos {
+	return t.Pos
+}
+
+func (t *ListNode) String() string {
+	return fmt.Sprintf("%s", t.Nodes)
+}
+
+func (t *ListNode) tree() *Tree {
+	return t.tr
 }
